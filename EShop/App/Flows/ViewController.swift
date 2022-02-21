626b8bf745a55784d6c0811c4a26175a82b6b355
NikitkaRylskiy <todoskatebaby@gmail.com>
2022-02-21 15:06:32 +0300

    Added auth business logic, login and user entities, request and error factories

--- EShop/App/Flows/ViewController.swift
+++ EShop/App/Flows/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let requestFactory = RequestFactory()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        authRequest()
     }
 
+    func authRequest() {
+        let auth = requestFactory.makeAuthRequestFatory()
+
+        auth.login(userName: "Somebody", password: "mypassword") { response in
+            switch response.result {
+                case .success(let login):
+                    print(login)
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+    }
 
 }
 
